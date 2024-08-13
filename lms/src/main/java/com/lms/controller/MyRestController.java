0d33765d2785c8456a296e9354049a91d26777b7
anandkumar1minda <anand.kumar1@mindacorporation.com>
2024-08-13 15:51:31 +0530

    send push

--- lms/src/main/java/com/lms/controller/MyRestController.java
+++ lms/src/main/java/com/lms/controller/MyRestController.java
@@ -1,7 +1,7 @@
 package com.lms.controller;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
