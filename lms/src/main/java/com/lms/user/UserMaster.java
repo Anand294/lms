eeb3342bb6358db7763f39d44d7d49f9e2ac77f9
anandkumar1minda <anand.kumar1@mindacorporation.com>
2024-08-16 11:37:29 +0530

    adding user

---
+++ lms/src/main/java/com/lms/user/UserMaster.java
@@ -0,0 +1,66 @@
+package com.lms.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "user_master")
+public class UserMaster {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	public int userId=0;
+	
+	
+	public String userName="";
+	public String name="";
+	public String emailId="";
+	public String password="";
+	public String userType="user";
+	public String Status="Active";
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getUserType() {
+		return userType;
+	}
+	public void setUserType(String userType) {
+		this.userType = userType;
+	}
+	public String getStatus() {
+		return Status;
+	}
+	public void setStatus(String status) {
+		Status = status;
+	}
+	
+}
---
+++ lms/src/main/java/com/lms/user/UserRepository.java
@@ -0,0 +1,7 @@
+package com.lms.user;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<UserMaster, Integer>{
+
+}
